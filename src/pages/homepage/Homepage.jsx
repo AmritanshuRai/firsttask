57d3a581e048751a397cfcd8bdff6a8099aa2b28
Amritanshu rai <amritanshurai13@gmail.com>
2021-08-13 03:20:42 +0530

    feat(sidenav): sidenav section added

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -8,23 +8,29 @@ const Homepage = () => {
   };
   return (
     <main>
-      <input
-        type="text"
-        onChange={handleInputChange}
-        value={letters}
-        placeholder="Type Your String"
-      />
-      <div className="homepage__container">
-        <div className="homepage__box">Box1</div>
-        <div className="homepage__box">Box2</div>
-        <div className="homepage__box">Box3</div>
-        <div className="homepage__box">Box4</div>
-        <div className="homepage__box">Box5</div>
-        <div className="homepage__box">Box6</div>
-        <div className="homepage__box">Box7</div>
-        <div className="homepage__box">Box8</div>
-        <div className="homepage__box">Box9</div>
-      </div>
+      <section className="sidenav">
+        <span className="close">-</span>
+        <div>sidenav</div>
+      </section>
+      <section className="major">
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={letters}
+          placeholder="Type Your String"
+        />
+        <div className="homepage__container">
+          <div className="homepage__box">Box1</div>
+          <div className="homepage__box">Box2</div>
+          <div className="homepage__box">Box3</div>
+          <div className="homepage__box">Box4</div>
+          <div className="homepage__box">Box5</div>
+          <div className="homepage__box">Box6</div>
+          <div className="homepage__box">Box7</div>
+          <div className="homepage__box">Box8</div>
+          <div className="homepage__box">Box9</div>
+        </div>
+      </section>
     </main>
   );
 };
