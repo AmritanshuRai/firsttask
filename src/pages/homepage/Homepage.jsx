1d2308a99343cc6afe6ef18e3a82e1390b8aff5d
Amritanshu rai <amritanshurai13@gmail.com>
2021-08-13 04:15:51 +0530

    feat(sidenav): sidenav close feature added

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -4,16 +4,22 @@ import "./homepage.styles.scss";
 
 const Homepage = () => {
   const [letters, setLetters] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
   const handleInputChange = (e) => {
     setLetters(e.target.value);
   };
+  const handleClose = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <main>
-      <section className="sidenav">
-        <span className="close">-</span>
-        <div>sidenav</div>
+      <section style={{ width: isOpen ? "20%" : "1%" }} className="sidenav">
+        <span onClick={handleClose} className="close">
+          -
+        </span>
+        <div>{isOpen && "Sidenav"}</div>
       </section>
-      <section className="major">
+      <section style={{ width: isOpen ? "80%" : "99%" }} className="major">
         <input
           type="text"
           onChange={handleInputChange}
